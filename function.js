7ca7570af97607ff0b84ed9bc925f6c5ba7cd9da
minhoCha <minhocha1004@gmail.com>
2019-02-21 17:30:49 -0800

    index and personal pages

---
+++ function.js
@@ -0,0 +1,9 @@
+function display1() {
+    const userName = document.getElementById('minho')
+    window.location.href = "/minho.html";
+ }
+
+ function display2() {
+    const userName = document.getElementById('Kayla')
+    window.location.href = "/Kay'sPage.html";
+ }
