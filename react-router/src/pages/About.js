05ab7176f24a2665e42502a4aa5ff190eed6aae1
Kayla Musleh <Kay@sfs-wifi-dhcp-10-143-221-74.wifi.sfsu.edu>
2019-04-29 16:18:55 -0700

    fixed commentary

--- react-router/src/pages/About.js
+++ react-router/src/pages/About.js
@@ -1,27 +1,19 @@
 import React from 'react';
-<<<<<<< HEAD
 import ListingCard from '../frontend/ListingCard'
 import Listing from '../frontend/Listing'
-=======
 import Listing from '../frontend/Listing';
 import NavbarResultPage from '../frontend/NavbarResultPage'
->>>>>>> e86fd37b4365e1cacc1512ede292b5e4c6be593d
 import { connect } from 'react-redux';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 
-
-
 const About = ({props}) => {
-    
     return (
-        
         <div>
-<<<<<<< HEAD
             <h2>About</h2>
             <Listing/>
-=======
+
             <NavbarResultPage />
             <Container fluid>
                 <Row>
@@ -31,7 +23,6 @@ const About = ({props}) => {
                     </Col>
                 </Row>
             </Container>
->>>>>>> e86fd37b4365e1cacc1512ede292b5e4c6be593d
         </div>
     );
 };
