4d27e65c8bbe91562c0de54bdede99fe904d5b20
Jgurdal <jgurdal@mail.sfsu.edu>
2019-05-01 16:20:18 -0700

    Replaced the old navbar from the homepage

--- react-router/src/pages/Home.js
+++ react-router/src/pages/Home.js
@@ -5,7 +5,7 @@ It will show this route when there's no specified path.
 import React from 'react';
 
 
-import NavbarComp from '../frontend/NavbarComp';
+import NavbarResultPage from '../frontend/NavbarResultPage';
 import Search from '../frontend/Search';
 import Container from 'react-bootstrap/Container';
 import Jumbotron from 'react-bootstrap/Jumbotron';
@@ -15,7 +15,7 @@ const Home = () => {
 
     return (
         <div>
-            <NavbarComp />
+            <NavbarResultPage />
         <Container fluid>
             <Jumbotron>
                 <Search />
