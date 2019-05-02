7855cac0afe38171209896de1a990ddf8ba77bd5
Rene Elias <reneelias03@gmail.com>
2019-05-01 17:58:28 -0700

    Reincluded the current navbar into the home page again.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,19 +4,23 @@ It will show this route when there's no specified path.
 
 import React from 'react';
 
-import NavbarComp from '../components/NavbarComp';
+
+import NavbarResultPage from '../components/NavbarResultPage';
 import Search from '../components/Search';
 import Container from 'react-bootstrap/Container';
 import Jumbotron from 'react-bootstrap/Jumbotron';
-import Image from 'react-bootstrap/Image';
 
 export const Home = () => {
-  return (
-    <Container fluid>
-      <NavbarComp />
-      <Jumbotron>
-        <Search />
-      </Jumbotron>
-    </Container>
-  );
+
+
+    return (
+        <div>
+            <NavbarResultPage />
+        <Container fluid>
+            <Jumbotron>
+                <Search />
+            </ Jumbotron>
+        </Container>
+        </div>
+    );
 };
