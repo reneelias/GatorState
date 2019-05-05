cca2bbc98291f460cba3a550ca05dbbd0b9df688
Kayla Musleh <Kay@sfs-wifi-dhcp-10-143-219-226.wifi.sfsu.edu>
2019-05-04 18:15:38 -0700

    fixed commenta

---
+++ client/src/pages/LoginPage.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import Listing from '../components/Listing';
+import NavbarResultPage from '../components/Navbar';
+import { connect } from 'react-redux';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+
+import axios from 'axios';
+
+
+class LoginPage extends Component {
+
+
+  render() {
+    return (
+      <div>
+        <h1>LoginPage</h1>
+      </div>
+    );
+  }
+}
+
+
+
+const mapStateToProps = state => {
+  // console.log(state);
+  return {
+
+  };
+};
+
+const mapDispatchToProps = {
+
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(LoginPage);
