253380483ac06465653076f34895beb8c5aed55a
Kayla Musleh <Kay@sfs-wifi-dhcp-10-143-211-165.wifi.sfsu.edu>
2019-05-08 17:20:45 -0700

    added " * " to error messages

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -57,14 +57,14 @@ handleSubmit(e) {
                     <label htmlFor="username">Username</label>
                     <input type="text" className="form-control" name="username" value={username} onChange={this.handleChange} />
                     {submitted && !username &&
-                        <div className="help-block">Username is required</div>
+                        <div className="help-block">*Username is required</div>
                     }
                 </div>
                 <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
                     <label htmlFor="password">Password</label>
                     <input type="password" className="form-control" name="password" value={password} onChange={this.handleChange} />
                     {submitted && !password &&
-                        <div className="help-block">Password is required</div>
+                        <div className="help-block">*Password is required</div>
                     }
                 </div>
                 <div className="form-group">
