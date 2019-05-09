6aeef82e28d41ed2a69210c4451b9f0b85915499
jgurdal <33738230+jgurdal@users.noreply.github.com>
2019-05-08 20:38:42 -0700

    Added documentation to the code

--- client/src/components/FilterSelection.js
+++ client/src/components/FilterSelection.js
@@ -1,23 +1,25 @@
 import React, {Component} from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
-
+//This component handles storing to the state what filter the user selected.
 class FilterSelection extends Component {
     constructor(props, context){
         super(props, context);
-
+        //Used to change the state value
         this.handleChange = this.handleChange.bind(this);
 
         this.state = {
             value: ""
         };
     }
-    handleChange(value, event){
-        this.setState({value});
+    handleChange(event){
+        this.setState({value: event.target.value});
     }
 
     render(){
         return(
+            //Dropdown selection that contains listing types
+            //when a value is selected we call onChange to handle the state change.
             <DropdownButton
                 type="checkbox"
                 value={this.state.value}
