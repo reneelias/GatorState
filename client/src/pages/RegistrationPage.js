6a3e486d5a1f0d701a0b95f0e7443230a4867366
Daisy Sun <itsdaisysun@gmail.com>
2019-05-19 21:28:41 -0700

    Fixed the register button, and added a checkbox for TOS

--- client/src/pages/RegistrationPage.js
+++ client/src/pages/RegistrationPage.js
@@ -8,25 +8,23 @@ const RegDiv = styled.div`
   margin: auto;
 `
 class RegistrationPage extends Component {
-
     constructor(props) {
         super(props);
-
+    
         this.state = {
             user: {
                 firstName: '',
                 lastName: '',
                 username: '',
-                password: '',
-                TOS: ''
+                password: ''
             },
             submitted: false
         };
-
+    
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
-
+    
     handleChange(event) {
         const { name, value } = event.target;
         const { user } = this.state;
@@ -37,99 +35,87 @@ class RegistrationPage extends Component {
             }
         });
     }
-
-    handleSubmit = async (event) => {
+    
+    handleSubmit(event) {
         event.preventDefault();
-
+    
         this.setState({ submitted: true });
         const { user } = this.state;
         const { dispatch } = this.props;
-
-        var urlString = `http://localhost:5000/users`;
-
-        console.log('user:')
-        console.log(user)
-
         if (user.firstName && user.lastName && user.username && user.password) {
             //dispatch(userActions.register(user));
-            await axios.post(urlString, user)
-                .then(function (response) {
-                    alert(response.data.message);
-                    console.log(response);
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
         }
     }
+    
+        render() {
+          const { registering  } = this.props;
+          const { user, submitted } = this.state;
+          return (
+              <div className="col-md-6 col-md-offset-3">
+                  <h2>Register</h2>
+                  <form name="form" onSubmit={this.handleSubmit}>
+                      <div className={'form-group' + (submitted && !user.firstName ? ' has-error' : '')}>
+                          <label htmlFor="firstName">First Name</label>
+                          <input type="text" className="form-control" name="firstName" value={user.firstName} onChange={this.handleChange} />
+                          {submitted && !user.firstName &&
+                              <div className="help-block">*First Name is required</div>
+                          }
+                      </div>
+                      <div className={'form-group' + (submitted && !user.lastName ? ' has-error' : '')}>
+                          <label htmlFor="lastName">Last Name</label>
+                          <input type="text" className="form-control" name="lastName" value={user.lastName} onChange={this.handleChange} />
+                          {submitted && !user.lastName &&
+                              <div className="help-block">*Last Name is required</div>
+                          }
+                      </div>
+                      <div className={'form-group' + (submitted && !user.username ? ' has-error' : '')}>
+                          <label htmlFor="username">Username</label>
+                          <input type="text" className="form-control" name="username" value={user.username} onChange={this.handleChange} />
+                          {submitted && !user.username &&
+                              <div className="help-block">*Username is required</div>
+                          }
+                      </div>
+                      <div className={'form-group' + (submitted && !user.password ? ' has-error' : '')}>
+                          <label htmlFor="password">Password</label>
+                          <input type="password" className="form-control" name="password" value={user.password} onChange={this.handleChange} />
+                          {submitted && !user.password &&
+                              <div className="help-block">*Password is required</div>
+                          }
+                      </div>
 
-    render() {
-        const { registering } = this.props;
-        const { user, submitted } = this.state;
-        return (
-            <div className="col-md-6 col-md-offset-3">
-                <form name="form" onSubmit={this.handleSubmit}>
-                    <div className={'form-group' + (submitted && !user.firstName ? ' has-error' : '')}>
-                        <input type="text" placeholder="First Name" className="form-control" name="firstName" value={user.firstName} onChange={this.handleChange} />
-                        {submitted && !user.firstName &&
-                            <div className="help-block">*First Name is required</div>
-                        }
+                    <div class="custom-control custom-checkbox mb-3">
+                        <input type="checkbox" class="custom-control-input" id="customControlValidation1" required></input>
+                        <label class="custom-control-label" for="customControlValidation1">Accept Terms of Service</label>
+                        <div class="invalid-feedback"> Invalid feedback </div>
                     </div>
-                    <div className={'form-group' + (submitted && !user.lastName ? ' has-error' : '')}>
-                        <input type="text" placeholder="Last Name" className="form-control" name="lastName" value={user.lastName} onChange={this.handleChange} />
-                        {submitted && !user.lastName &&
-                            <div className="help-block">*Last Name is required</div>
-                        }
-                    </div>
-                    <div className={'form-group' + (submitted && !user.username ? ' has-error' : '')}>
-                        <input type="text" placeholder="Username"  className="form-control" name="username" value={user.username} onChange={this.handleChange} />
-                        {submitted && !user.username &&
-                            <div className="help-block">*Username is required</div>
-                        }
-                    </div>
-                    <div className={'form-group' + (submitted && !user.password ? ' has-error' : '')}>
-                        <input type="password" placeholder="Password"  className="form-control" name="password" value={user.password} onChange={this.handleChange} />
-                        {submitted && !user.password &&
-                            <div className="help-block">*Password is required</div>
-                        }
-                    </div>
-{/* terms of service checkbox */}
-                     <div className={'form-group' + (submitted ? ' has-error' : '')}>
-                        <label htmlFor="TOS"></label>
-                        <input type="checkbox" className="form-control" name="TOS" onChange={this.handleChange} >I have read the Terms of Service.</input>
-                        {submitted && !user.TOS &&
-                            <div className="help-block">*required</div>
-                        }
-                    </div>
-{/* terms of service checkbox */}
-                    <div className="form-group">
-                        <button className="btn btn-primary">Register</button>
-                        {registering &&
-                            <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
-                        }
-
-                    </div>
-                </form>
-            </div>
-        );
-    }
-}
-
-
-
-
-const mapStateToProps = state => {
-    // console.log(state);
-    return {
-
+                      <div className="form-group">
+                          <button className="btn btn-primary">Register</button>
+                          {registering && 
+                              <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
+                          }
+       
+                      </div>
+                  </form>
+              </div>
+          );
+      }
+      }
+    
+    
+    
+    
+    const mapStateToProps = state => {
+      // console.log(state);
+      return {
+    
+      };
     };
-};
-
-const mapDispatchToProps = {
-
-};
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
+    
+    const mapDispatchToProps = {
+    
+    };
+    
+    export default connect(
+      mapStateToProps,
+      mapDispatchToProps
 )(RegistrationPage);
