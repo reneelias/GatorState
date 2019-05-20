bf678df16b08019f7356c1dae37bc59a87b0c29e
Daisy Sun <itsdaisysun@gmail.com>
2019-05-20 14:39:52 -0700

    Contact seller button

--- client/src/components/ListingModal.js
+++ client/src/components/ListingModal.js
@@ -3,7 +3,9 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import ContactModal from '../components/ContactModal';
 import Col from 'react-bootstrap/Col';
+import RegisterModal from './RegisterModal';
 
 export default class ListingModal extends Component{
     constructor(props, context){
@@ -49,7 +51,9 @@ export default class ListingModal extends Component{
                 </Container>
             </Modal.Body>
             <Modal.Footer>
-                <Button>Contact Seller</Button>
+                {/* <Button> Contact Seller </Button> */}
+                <ContactModal/>
+                
             </Modal.Footer>
             </Modal>
             </div>
