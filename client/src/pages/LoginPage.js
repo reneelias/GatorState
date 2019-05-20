38326589d01af592ecbe54e3546c2e2d401c109d
minhoCha <minhocha1004@gmail.com>
2019-05-19 20:32:10 -0700

    Login modal css updated

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -106,18 +106,15 @@ class LoginPage extends Component {
         const { username, password, submitted } = this.state;
         return (
             <ModalContent className="col-md-6 col-md-offset-3">
-                <h2>Login</h2>
                 <form name="form" onSubmit={this.handleSubmit}>
                     <div className={'form-group' + (submitted && !username ? ' has-error' : '')}>
-                        <label htmlFor="username">Username</label>
-                        <input type="text" className="form-control" name="username" value={username} onChange={this.handleChange} />
+                        <input type="text" placeholder="Username" className="form-control" name="username" value={username} onChange={this.handleChange} />
                         {submitted && !username &&
                             <div className="help-block">*Username is required</div>
                         }
                     </div>
                     <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
-                        <label htmlFor="password">Password</label>
-                        <input type="password" className="form-control" name="password" value={password} onChange={this.handleChange} />
+                        <input type="password" placeholder="Password" className="form-control" name="password" value={password} onChange={this.handleChange} />
                         {submitted && !password &&
                             <div className="help-block">*Password is required</div>
                         }
